46ffb4c84e4dfcb93fb40a070eb69d0edc3d9f1a
kirumipat <kirumipat@gmail.com>
2022-05-30 15:06:28 +0300

    12

--- arbio.sh
+++ arbio.sh
@@ -29,7 +29,7 @@ mount dev/sda1 /mnt/boot/EFI
 pacman -Syy --noconfirm
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "[chaotic-aur]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 pacman -Syy --noconfirm
