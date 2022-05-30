2f21d1c9cef68dac7260df7aad101e809049580f
kirumipat <kirumipat@gmail.com>
2022-05-30 15:22:38 +0300

    14

--- arbio.sh
+++ arbio.sh
@@ -26,16 +26,25 @@ mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@cashe /d
 mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@log /dev/sda2  /mnt/var/log
 mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/sda2  /mnt/var/tmp
 mount dev/sda1 /mnt/boot/EFI
+
+sed '/ru_RU.UTF-8 UTF-8/s/^#//' -i /etc/locale.gen
+locale-gen
 pacman -Syy --noconfirm
+
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
 echo "[liquorix]" >> /etc/pacman.conf
 echo "Server = https://liquorix.net/archlinux/$repo/$arch" >> /etc/pacman.conf
+sudo pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
+sudo pacman-key --lsign-key 9AE4078033F8024D
+
 pacman -Syy --noconfirm
 pacstrap /mnt base base-devel btrfs-progs mkinitcpio-btrfs mkinitcpio-firmware linux-lqx linux-lqx-headers linux-lqx-docs linux-firmware grub os-prober networkmanager efibootmgr dosfstools mtools go xorg gnome nano wget curl git
+
 genfstab -U -p /mnt >> /mnt/etc/fstab
 sed '/ru_RU.UTF-8 UTF-8/s/^#//' -i /mnt/etc/locale.gen
 echo "KEYMAP=ru" >> /mnt/etc/vconsole.conf
