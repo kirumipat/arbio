39eb167105452347945a21869ac892debc003128
kirumipat <kirumipat@gmail.com>
2022-05-30 16:06:40 +0300

    22

--- arbiostart.sh
+++ arbiostart.sh
@@ -45,7 +45,8 @@ ln -s /usr/share/zoneinfo/Europe/Kiev /mnt/etc/localtime
 echo "localhost" >> /mnt/etc/hostname
 sed '/%wheel ALL=(ALL) All/s/^#//' -i /mnt/etc/sudoers
 
-cp /arbio/in.sh /mnt/home
+cp /arbio/in.sh /mnt/home/
+cp /arbio/addrepo.sh /mnt/home/
 arch-chroot /mnt
 
 
