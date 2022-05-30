562a5dd0e97364d4a2e2f0bf2106d01331b1fa2a
kirumipat <kirumipat@gmail.com>
2022-05-30 18:01:52 +0300

    26

--- arbiostart.sh
+++ arbiostart.sh
@@ -6,36 +6,36 @@ mkfs.fat -F32 -n BOOT /dev/sda1
 mkfs.btrfs -f -L ROOT /dev/sda2
 
 mount /dev/sda2 /mnt
-cd /mnt
 
-btrfs su cr @
-btrfs su cr @home
-btrfs su cr @root
-btrfs su cr @srv
-btrfs su cr @cashe
-btrfs su cr @log
-btrfs su cr @tmp
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@root
+btrfs su cr /mnt/@var
+btrfs su cr /mnt/@opt
+btrfs su cr /mnt/@tmp
+btrfs su cr /mnt/@.snapshots
+
 umount /dev/sda2
+
 mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@ /dev/sda2  /mnt/
-mkdir  /mnt/{boot,home,root,srv,var}
-mkdir /mnt/boot/EFI
-cd /mnt/var
-mkdir {cashe,log,tmp}
-cd ../
+mkdir  /mnt/{boot,home,root,var,opt,tmp,.snapshots}
+#mkdir /mnt/boot/EFI
 mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@home /dev/sda2  /mnt/home
 mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@root /dev/sda2  /mnt/root
-mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@srv /dev/sda2  /mnt/srv
-mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@cashe /dev/sda2  /mnt/var/cashe
-mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@log /dev/sda2  /mnt/var/log
-mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/sda2  /mnt/var/tmp
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@var /dev/sda2  /mnt/var
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@opt /dev/sda2  /mnt/opt
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@.snapshots /dev/sda2  /mnt/.snapshots
+mount -o noatime,compress=zstd,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/sda2  /mnt/tmp
 mount dev/sda1 /mnt/boot/EFI
 
 sed '/ru_RU.UTF-8 UTF-8/s/^#//' -i /etc/locale.gen
 locale-gen
+localectl set-locale ru_RU.UTF-8
+
 pacman -Syy --noconfirm
 
 pacman -Syy --noconfirm
-pacstrap /mnt base base-devel btrfs-progs linux-zen linux-zen-headers linux-zen-docs linux-firmware grub os-prober networkmanager efibootmgr dosfstools mtools go xorg gnome nano wget curl git
+pacstrap /mnt base base-devel btrfs-progs intel-ucode linux-zen linux-zen-headers linux-zen-docs linux-firmware grub os-prober networkmanager efibootmgr dosfstools mtools go xorg gnome gnome-shell-extensions nano wget curl git
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 sed '/ru_RU.UTF-8 UTF-8/s/^#//' -i /mnt/etc/locale.gen
