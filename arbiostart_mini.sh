2b3f17b45451750c1f1092171f9d5f3d701191d2
Roman Sam <49292090+kirumipat@users.noreply.github.com>
2022-07-06 19:37:11 +0300

    Update arbiostart_mini.sh

--- arbiostart_mini.sh
+++ arbiostart_mini.sh
@@ -5,10 +5,10 @@ lsblk
 read -p "Disk =  " AddDisk
 echo -e "n\np\n1\n\n+1024M\nn\np\n2\n\n\na\n1\nw\n" | fdisk /dev/$AddDisk
 #Форматирование разделов
-mkfs.fat -F32 -n BOOT /dev/$AddDisk + '1'
-mkfs.btrfs -f -L ROOT /dev/$AddDisk + '2'
+mkfs.fat -F32 -n BOOT /dev/($AddDisk + '1')
+mkfs.btrfs -f -L ROOT /dev/($AddDisk + '2')
 #Монтируем раздел
-mount /dev/$AddDisk + '2' /mnt
+mount /dev/($AddDisk + '2') /mnt
 #Создаём BTRFS тома
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
@@ -18,17 +18,17 @@ btrfs su cr /mnt/@opt
 btrfs su cr /mnt/@tmp
 btrfs su cr /mnt/@snapshots
 #Отмантируем раздел
-umount /dev/$AddDisk + '2'
+umount /dev/($AddDisk + '2')
 #Монтируем тома в разделы со сжатием и свойствами
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@ /dev/$AddDisk + '2'  /mnt/
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@ /dev/($AddDisk + '2')  /mnt/
 mkdir -p /mnt/{boot/EFI,home,root,var,opt,tmp,.snapshots}
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@home /dev/$AddDisk + '2'  /mnt/home
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@root /dev/$AddDisk + '2'  /mnt/root
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@var /dev/$AddDisk + '2'  /mnt/var
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@opt /dev/$AddDisk + '2'  /mnt/opt
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@snapshots /dev/$AddDisk + '2'  /mnt/.snapshots
-mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/$AddDisk + '2'  /mnt/tmp
-mount /dev/$AddDisk + '1' /mnt/boot/EFI
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@home /dev/($AddDisk + '2')  /mnt/home
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@root /dev/($AddDisk + '2')  /mnt/root
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@var /dev/($AddDisk + '2') /mnt/var
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@opt /dev/($AddDisk + '2')  /mnt/opt
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@snapshots /dev/($AddDisk + '2')  /mnt/.snapshots
+mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/($AddDisk + '2')  /mnt/tmp
+mount /dev/($AddDisk + '1') /mnt/boot/EFI
 #Установка минимального набора
 sed 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' -i /etc/pacman.conf
 pacman -Syy --noconfirm
