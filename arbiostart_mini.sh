aa8924200955ce11546bab94d41d1d4dfc2b4bf1
Roman Sam <49292090+kirumipat@users.noreply.github.com>
2022-07-07 22:12:14 +0300

    Update arbiostart_mini.sh

--- arbiostart_mini.sh
+++ arbiostart_mini.sh
@@ -9,6 +9,7 @@ echo -e "n\np\n1\n\n+1024M\nn\np\n2\n\n\na\n1\nw\n" | fdisk /dev/$AddDisk
 #Форматирование разделов
 mkfs.fat -F32 -n BOOT /dev/$DN1
 mkfs.btrfs -f -L ROOT /dev/$DN2
+#mkfs.btrfs -f -L SHARA -m raid5 -d raid5 /dev/sda /dev/sdb /dev/sdc /dev/sdd
 #Монтируем раздел
 mount /dev/$DN2 /mnt
 #Создаём BTRFS тома
@@ -24,13 +25,14 @@ btrfs su cr /mnt/@shara
 umount /dev/$DN2
 #Монтируем тома в разделы со сжатием и свойствами
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@ /dev/$DN2  /mnt/
-mkdir -p /mnt/{boot/EFI,home,root,var,opt,tmp,.snapshots}
+mkdir -p /mnt/{boot/EFI,home,root,var,opt,tmp,.snapshots,shara}
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@home /dev/$DN2  /mnt/home
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@root /dev/$DN2  /mnt/root
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@var /dev/$DN2 /mnt/var
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@opt /dev/$DN2  /mnt/opt
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@snapshots /dev/$DN2  /mnt/.snapshots
 mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@tmp /dev/$DN2  /mnt/tmp
+#mount -o noatime,compress=zstd:2,ssd,space_cache=v2,discard=async,subvol=@shara /dev/sda  /mnt/shara
 mount /dev/$DN1 /mnt/boot/
 #Установка минимального набора
 sed 's/Architecture = auto/Architecture = auto \n ILoveCandy/g' -i /etc/pacman.conf
